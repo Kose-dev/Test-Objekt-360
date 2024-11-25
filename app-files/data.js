11098cbc7fe26f6a46e8f0d6bbe62f3acf7e1e88
Kose-dev <kolb.productions.germany@gmail.com>
2024-11-25 17:05:26 +0100

    upload

---
+++ app-files/data.js
@@ -0,0 +1,60 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20241125_164047_00_007",
+      "name": "IMG_20241125_164047_00_007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.021132533504619744,
+          "pitch": 0.2689766305587806,
+          "rotation": 6.283185307179586,
+          "target": "0-img_20241125_164047_00_007"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0740680833759058,
+          "pitch": -0.019639050148626325,
+          "title": "Flur",
+          "text": "Testbeschriebung"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
